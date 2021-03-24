4a658584cd423fbca1fe549421d77d0b46b7bd89
manuvargasg <81274432+manuvargasg@users.noreply.github.com>
2021-03-23 20:54:50 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB6oguUif584O1ODFuSVzMh9C2KtI391do",
+    authDomain: "autentifica42lmvg.firebaseapp.com",
+    projectId: "autentifica42lmvg",
+    storageBucket: "autentifica42lmvg.appspot.com",
+    messagingSenderId: "907644660308",
+    appId: "1:907644660308:web:badca002031fd5aa4d5bc1",
+    measurementId: "G-W0GS2TFH3N"
+    });
